32507f9a10e8929a678a34277e65fcbbf48c842b
abzhestovskyy <abzhestovskyy@logixits.com>
2021-09-04 19:29:19 +0300

    second commit

---
+++ src/test/java/com/graphql/example/tests/FirstGraphQLTest.java
@@ -0,0 +1,13 @@
+package com.graphql.example.tests;
+
+import org.junit.jupiter.api.Test;
+
+public class FirstGraphQLTest {
+
+    @Test
+    public void firstTest(){
+
+        var query ="{\"query\": \"{ company { ceo coo cto } }\"}";
+
+    }
+}
